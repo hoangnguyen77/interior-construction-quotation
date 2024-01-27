9201c07b77691e2a50ec5851b20567eb8885f6ec
s3rcc <s3rcc.9@gmail.com>
2024-01-28 01:54:15 +0700

    Add addBlog function

--- src/main/java/com/swp/spring/interiorconstructionquotation/controller/BlogController.java
+++ src/main/java/com/swp/spring/interiorconstructionquotation/controller/BlogController.java
@@ -5,9 +5,7 @@ import com.swp.spring.interiorconstructionquotation.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,6 +39,15 @@ public class BlogController {
             return "error/404";
         }
     }
-
+    @GetMapping("/add")
+    public String showAddBlogForm(Model model){
+        model.addAttribute("blog", new Blog());
+        return "blog/add-form";
+    }
+    @PostMapping("/add")
+    public String addBlog(@ModelAttribute("blog") Blog blog ){
+        blogService.createBlog(blog);
+        return "redirect:/blogs";
+    }
 
 }
